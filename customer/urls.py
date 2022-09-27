377def9679d97828e80de6aca55ff7ec05d8ef52
rahulrdk <rahul.v.m.ponnu@gmail.com>
2022-09-27 22:00:32 +0530

    4th commit

--- customer/urls.py
+++ customer/urls.py
@@ -6,6 +6,8 @@ app_name='customer'
 urlpatterns = [
 
     path('my_order',views.my_order,name='my_order'),
+    path('check_out',views.check_out,name='check_out'),
+    path('my_cart',views.my_cart,name='my_cart'),
 
     
 ]
--- customer/views.py
+++ customer/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def my_order(request):
     return render(request,'customer/my_order.html')
+
+def check_out(request):
+    return render(request,'customer/check_out.html')
+
+def my_cart(request):
+    return render(request,'customer/my_cart.html')
